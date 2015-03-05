33f8d2fa2e560572d76cb5d48591aa37ac1494b1
jacques <me@jacquesgiraudel.com>
2015-03-05 20:22:30 +0100

    Added TP Onglets fixes

---
+++ 3 - Design/TP MultiPane sans loader/app/src/main/java/com/jgl/tpmultipane/DetailContactActivity.java
@@ -0,0 +1,25 @@
+package com.jgl.tpmultipane;
+
+import android.app.Fragment;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+
+public class DetailContactActivity extends ActionBarActivity{
+
+    public static final String EXTRA_CONTACT_ID = "contact_id";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_contact);
+    }
+
+}
